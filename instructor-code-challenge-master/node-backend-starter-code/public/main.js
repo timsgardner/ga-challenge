0c3f98d7d58ef2b6a63733d50ba8aeb17d51f085
timsgardner <timsgardner@gmail.com>
2016-04-14 11:22:57 -0400

    commenting and cleaning up

--- instructor-code-challenge-master/node-backend-starter-code/public/main.js
+++ instructor-code-challenge-master/node-backend-starter-code/public/main.js
@@ -1,7 +1,8 @@
-console.log('here we are again');
+// ============================================================
+// dom manipulation conveniences
 
-var stuff = 'trouse';
-
+// creates a dom element with the specified tag; further arguments are
+// attached as classes.
 function el(tag){
     var elmt = $(document.createElement(tag));
     for (var i=1; i < arguments.length; i++){
@@ -10,13 +11,8 @@ function el(tag){
     return elmt;
 }
 
-function pdiv(text){
-    return el('p').append(text);
-}
-
-function span(text){
-    return el('span').append(text);
-}
+// ============================================================
+// functions for constructing the table of results
 
 function resultElementProperty(val){
     return el('td').append(val);
@@ -25,45 +21,48 @@ function resultElementProperty(val){
 function resultElement(result, colkeys){
     var row = el('tr');
     var elementDivs = colkeys
-	.map(function(k){return result[k];})
-	.map(resultElementProperty);
+						.map(function(k){return result[k];})
+						.map(resultElementProperty);
     row.append(elementDivs);
     return row;
 }
 
-var injectResultsLog;
+// ============================================================
+// functions for constructing the detail view of clicked movies
 
+// make a button to press to register that the user wants to save a movie 
 function favoriteButton(title){
     var button = el('button').attr('type', 'submit').append('Favorite!');
     var form = el('form', 'buttonForm').attr('method', 'post').append(button);
     form.submit(function(event){
-	console.log('submitting!' + title);
-	$.post('/favorite',
-	       {title: title},
-	       function(x){
-		   // really just a put at the moment 
-	       });
-	event.preventDefault();
+		$.post('/favorite',
+		       {title: title},
+		       function(x){
+			   // really just a put at the moment 
+		       });
+		event.preventDefault();
     });
     return form;
 }
 
+// render various fields of interest in the more detailed data
 function detailBox(data){
     var detailSectioner = function(k, label){
-	var sec = el('div', 'detailSection');
-	sec.append(el('h5', 'detailSectionLabel').append(label));
-	sec.append(el('div', 'detailSectionContents').append(data[k]))
-	return sec;
-    };
+			var sec = el('div', 'detailSection');
+			sec.append(el('h5', 'detailSectionLabel').append(label));
+			sec.append(el('div', 'detailSectionContents').append(data[k]))
+			return sec;
+	    };
     return el('div', 'detailBox')
-	.append(detailSectioner("Title", "Title"))
-	.append(detailSectioner("Type", "Medium"))
-    	.append(detailSectioner("Released", "Released"))
-	.append(detailSectioner("Runtime", "Runtime"))
-    	.append(detailSectioner("Plot", "Plot"))
-	.append(favoriteButton(data.Title));
+		.append(detailSectioner("Title", "Title"))
+		.append(detailSectioner("Type", "Medium"))
+	   	.append(detailSectioner("Released", "Released"))
+		.append(detailSectioner("Runtime", "Runtime"))
+	   	.append(detailSectioner("Plot", "Plot"))
+		.append(favoriteButton(data.Title));
 }
 
+// given an imdbID, get and render more detailed information on it from omdb
 function showDetails(id){
     $.post('/details', {id: id},
 	  function(data){
@@ -72,35 +71,31 @@ function showDetails(id){
 	  });
 }
 
-var clickedThings = [];
+// ============================================================
+// top; render results of query
 
+// render the table of results
 function injectResults(results){
-    injectResultsLog = results;
     var colkeys = ['Title', 'Year', 'imdbID'];
     var elements = results.map(function(r){return resultElement(r,colkeys);});
-    // click handlers
+    // click handlers; trigger the detailBox
     elements.forEach(function(elmt){
-	$(elmt).click(function(event){
-	    clickedThings.push(this);
-	    showDetails($(this).find('td').last().text());
-	});
-    });
+			$(elmt).click(function(event){
+			    showDetails($(this).find('td').last().text());
+			});
+	    });
     var header = el('tr').append(colkeys.map(function(k){return el('th').append(k);}))
     var tab = el('table', 'resultTable').append(header).append(elements);
     $('#results').empty().append(tab);
 }
 
-var submissionLog;
-var submissionResults;
-
+//============================================================
+// init
 $(function(){
     $('form[id=search]').submit(function(event){
-	submissionLog = {event:event, dataStuff:$(this)}
 	$.post('/searching', 
 	       $(this).serialize(),
 	       function(x){
-		   submissionResults = x;
-		   console.log('got stuff back; results: ' + x);
 		   injectResults(x);});
 	$('#results').empty().append(el('div','spinning').append('please wait'));
 	event.preventDefault();
